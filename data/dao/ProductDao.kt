aef0a66333f6fe1f751c2d653f71c52e9e39c7f3
osvaldoamg95-afk <osvaldoamg95@gmail.com>
2026-02-24 18:42:18 -0500

    Add ProductDao interface for product operations

---
+++ data/dao/ProductDao.kt
@@ -0,0 +1,21 @@
+package com.tunegocio.app.data.dao
+
+import androidx.room.*
+import com.tunegocio.app.data.entities.Product
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductDao {
+
+    @Insert
+    suspend fun insert(product: Product)
+
+    @Update
+    suspend fun update(product: Product)
+
+    @Delete
+    suspend fun delete(product: Product)
+
+    @Query("SELECT * FROM products ORDER BY name ASC")
+    fun getAll(): Flow<List<Product>>
+}
