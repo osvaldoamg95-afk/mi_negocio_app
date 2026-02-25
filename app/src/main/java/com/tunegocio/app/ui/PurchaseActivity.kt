2eb595cc1d43f0b5026207cc4411e189c7e7dca1
osvaldoamg95-afk <osvaldoamg95@gmail.com>
2026-02-24 20:46:51 -0500

    Add import for AppDatabase in PurchaseActivity

--- app/src/main/java/com/tunegocio/app/ui/PurchaseActivity.kt
+++ app/src/main/java/com/tunegocio/app/ui/PurchaseActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
+
 import com.tunegocio.app.data.AppDatabase
 import com.tunegocio.app.data.entities.InventoryLot
 import com.tunegocio.app.databinding.ActivityPurchaseBinding
