4f2acbf89c466eea318d0a7e927babf88dea3ae9
osvaldoamg95-afk <osvaldoamg95@gmail.com>
2026-02-26 17:24:30 -0500

    Update RawMaterial.kt

--- data/entities/RawMaterial.kt
+++ data/entities/RawMaterial.kt
@@ -1,3 +1,8 @@
+package com.tunegocio.app.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "raw_materials")
 data class RawMaterial(
     @PrimaryKey(autoGenerate = true)
