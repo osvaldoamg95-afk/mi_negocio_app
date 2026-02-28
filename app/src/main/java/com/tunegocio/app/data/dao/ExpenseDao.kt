2dca192fdb49314aedeabcd75eae585b83e0af7b
osvaldoamg95-afk <osvaldoamg95@gmail.com>
2026-02-27 19:15:36 -0500

    Update ExpenseDao.kt

--- app/src/main/java/com/tunegocio/app/data/dao/ExpenseDao.kt
+++ app/src/main/java/com/tunegocio/app/data/dao/ExpenseDao.kt
@@ -9,6 +9,9 @@ interface ExpenseDao {
     @Insert
     suspend fun insert(expense: Expense)
 
+    @Query("SELECT * FROM expenses ORDER BY date DESC")
+    suspend fun getAllExpenses(): List<Expense>
+
     @Query("""
         SELECT SUM(amount) FROM expenses
         WHERE date BETWEEN :startDate AND :endDate
